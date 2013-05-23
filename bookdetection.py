0b27932c67acfadca8ad1431907e2a78f2f73123
Ramón González <ramonesteban.gzz@gmail.com>
2013-05-23 05:16:52 -0500

    Book detection

--- bookdetection.py
+++ bookdetection.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 from tkFileDialog import askopenfilename
 from pytesser import *
+from squares import *
 import Image, ImageTk, webbrowser
 
 class Interface:
@@ -10,14 +11,14 @@ class Interface:
         self.root.resizable(width=False, height=False)
 
         self.labeltext = StringVar()
-        self.labeltext.set('Selecciona una imagen')
-        self.label = Label(self.root, textvariable=self.labeltext)
-        self.label.pack(side=TOP)
+        self.labeltext.set('No hay imagen seleccionada')
+        self.label = Label(self.root, textvariable=self.labeltext).grid(row=0, column=1, columnspan=3)
+        self.label = Label(self.root, text='Vista previa:').grid(row=1, column=0)
 
-        self.button_search = Button(text='Buscar Archivo', width=10, command=self.select_file).pack(side=TOP)
-        self.button_detect = Button(text='Abrir', width=10, command=self.openit).pack(side=TOP)
-        self.button_detect = Button(text='Detectar', width=10, command=self.action).pack(side=TOP)
-        self.button_exit = Button(text='Salir', width=10, command=self.root.destroy).pack(side=TOP)
+        self.button_search = Button(text='Seleccionar Archivo', width=15, command=self.select_file).grid(row=0, column=0)
+        self.button_detect = Button(text='Abrir', width=12, command=self.openit).grid(row=1, column=1)
+        self.button_detect = Button(text='Detectar', width=12, command=self.action).grid(row=1, column=2)
+        self.button_exit = Button(text='Salir', width=12, command=self.root.destroy).grid(row=1, column=3)
 
     def select_file(self):
         self.filename = askopenfilename(filetypes=[('allfiles', '*'), ('images', '*.jpg')])
@@ -25,21 +26,91 @@ class Interface:
 
     def openit(self):
         image = Image.open(self.filename)
-        image = image.resize((400, 400), Image.ANTIALIAS)
+        image = image.resize((140, 140), Image.ANTIALIAS)
         self.imagetk = ImageTk.PhotoImage(image)
-        self.labelimage = Label(self.root, image=self.imagetk)
-        self.labelimage.pack(side=LEFT)
+        self.labelimage = Label(self.root, image=self.imagetk).grid(row=2, column=0)
 
     def action(self):
-        text = image_file_to_string(self.filename)
-        text = text[:24]
-        self.labelimage = Label(text=text, foreground='#00f')
-        self.labelimage.bind('<1>', lambda event, text=text: self.click_link(event, text))
-        self.labelimage.pack()
+        squares = search(self.filename)
+        booksdetected = self.cut(self.filename, squares)
+
+        image = Image.open('preview.png')
+        image = image.resize((140, 140), Image.ANTIALIAS)
+        self.imagetk = ImageTk.PhotoImage(image)
+        self.labelimage = Label(self.root, image=self.imagetk).grid(row=2, column=0)
+
+        r = 2
+        c = 0
+        self.bookimagetk = [1, 2, 3, 4]
+        self.bookimage = [1, 2, 3, 4]
+        for num in booksdetected:
+            bookfile = 'frontbook-%d.png' % num
+            text = image_file_to_string(bookfile)
+            text = text[:26]
+            print text
+
+            image = Image.open(bookfile)
+            image = image.resize((140, 140), Image.ANTIALIAS)
+            self.bookimagetk[c] = ImageTk.PhotoImage(image)
+            self.bookimage[c] = Label(self.root, image=self.bookimagetk[c]).grid(row=r, column=1, columnspan=2)
+
+            self.linkimage = Label(text=text, foreground='#00f')
+            self.linkimage.bind('<1>', lambda event, text=text: self.click_link(event, text))
+            self.linkimage.grid(row=r, column=3)
+            r += 1
+            c += 1
 
     def click_link(self, event, text):
         google_link = 'http://www.google.es/search?btnG=Buscar+libros&tbm=bks&tbo=1&hl=es&oq=ma&q=' + text
         webbrowser.open(google_link)
+
+    def cut(self, image, squares):
+        image = Image.open(image)
+        width, height = image.size
+
+        frontcount = 0
+        space = 60
+        lastsizes = []
+        repeated = []
+        for square in squares:
+            p1 = square[0]
+            p2 = square[1]
+            p3 = square[2]
+            p4 = square[3]
+
+            pixels = image.load()
+            an = (p3[0]-p1[0])
+            al = (p3[1]-p1[1])
+
+            for size in lastsizes:
+                w, h = size
+                if an > w-space and an < w+space and al > h-space and al < h+space:
+                    if frontcount in repeated:
+                        pass
+                    else:
+                        repeated.append(frontcount)
+                    
+            if an > (width*0.1) and al > (height*0.1) and an < (width*0.9) and al < (height*0.9):
+                lastsizes.append((an, al))
+                newimage = Image.new('RGB', (an, al), (255, 255, 255))
+                pix = newimage.load()
+                x = 0
+                y = 0
+                for i in range(p1[0], p3[0]):
+                    for j in range(p1[1], p3[1]):
+                        pix[x, y] = pixels[i, j]
+                        y += 1
+                    x += 1
+                    y = 0
+                imagename = 'frontbook-%d.png' % frontcount
+                frontcount += 1
+                newimage.save(imagename)
+
+        booksdetected = []
+        for element in range(frontcount-1):
+            if element not in repeated:
+                booksdetected.append(element)
+        return booksdetected
 
 def main():
     root = Tk()
--- squares.py
+++ squares.py
@@ -1,13 +1,18 @@
 import numpy as np
 import cv2
+import Image
 
 def angle_cos(p0, p1, p2):
     d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
-    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+    return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1)*np.dot(d2, d2)))
 
-def find_squares(img):
+def find_squares(image_path):
+    img = cv2.imread(image_path)
     img = cv2.GaussianBlur(img, (5, 5), 0)
+    onesquare = []
     squares = []
+    preview = []
+
     for gray in cv2.split(img):
         for thrs in xrange(0, 255, 26):
             if thrs == 0:
@@ -15,7 +20,7 @@ def find_squares(img):
                 bin = cv2.dilate(bin, None)
             else:
                 retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
-            bin, contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
             for cnt in contours:
                 cnt_len = cv2.arcLength(cnt, True)
                 cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
@@ -23,13 +28,20 @@ def find_squares(img):
                     cnt = cnt.reshape(-1, 2)
                     max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
                     if max_cos < 0.1:
-                        squares.append(cnt)
+                        onesquare.append((cnt[0][0], cnt[0][1]))
+                        onesquare.append((cnt[1][0], cnt[1][1]))
+                        onesquare.append((cnt[2][0], cnt[2][1]))
+                        onesquare.append((cnt[3][0], cnt[3][1]))
+                        squares.append(onesquare)
+                        onesquare = []
+                        preview.append(cnt)
+    return squares, preview
+
+def search(image_path):
+    squares, preview = find_squares(image_path)
+    image = cv2.imread(image_path)
+    cv2.drawContours(image, preview, -1, (0, 255, 0), 3)
+    newimage = Image.fromarray(image)
+    newimage.save('preview.png')
     return squares
 
-if __name__ == '__main__':
-    from glob import glob
-    for fn in glob('./pic*.png'):
-        img = cv2.imread(fn)
-        squares = find_squares(img)
-        cv2.drawContours( img, squares, -1, (0, 255, 0), 3 )
-        cv2.imshow('squares', img)
